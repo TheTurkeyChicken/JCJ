cbd81115dd00f08977b370ebdfca1fbd837ba662
Chris Liu <LitLiu0418@gmail.com>
2021-07-18 12:56:10 -0700

    Update file
    chris's commit

--- helloworld.java
+++ helloworld.java
@@ -1,7 +1,7 @@
 class Main {
 	public static void main (String[] args) {
 		System.out.print("Hello Chris.");
-		System.out.print("Hello Justin.");
-		System.out.print("Hello Julia.");
+		System.out.println("Hello Justin.");
+		System.out.println("Hello Julia.");
 	}
 }
